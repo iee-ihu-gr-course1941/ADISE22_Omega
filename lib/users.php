269c12e9b303cf52917996ac8dbb9d2f704991e9
thomasoikon96 <57902855+thomasoikon96@users.noreply.github.com>
2022-12-20 18:34:30 +0200

    Create users.php

---
+++ lib/users.php
@@ -0,0 +1,14 @@
+<?php
+
+function show_users() {
+	global $mysqli;
+	$sql = 'select username,playerId from players';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	header('Content-type: application/json');
+	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
+}
+
+
+?>
