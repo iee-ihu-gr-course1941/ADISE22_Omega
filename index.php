55b8d73bae1c00a4dc717cf2d6288dcf195b31aa
kokoshkas007 <58370775+kokoshkas007@users.noreply.github.com>
2023-01-04 00:50:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,169 +2,187 @@
 require_once "php/dbconnect.php";
 
 #require_once "php/board.php";
-$method=$_SERVER['REQUEST_METHOD'];
-$input=json_decode(file_get_contents('php://input'),true);
+$method = $_SERVER['REQUEST_METHOD'];
+$requestBody = json_decode(file_get_contents('php://input'), true);
 
 $request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 $resource = $request[0];
 
+print_r($requestBody);
+//$subresource = $request[1];
+
 #print "\n METHOD " . $method .  "\n";
 
 #print "\n REQUEST_BODY \n";
-#print_r($input);
+#print_r($requestBody);
 #print "\n REQUEST RESOURCE\n";
 #print_r($resource);
 
 
 
-if ($resource == "players") {
-	if ($method == "POST") {
-		print "Posting players resource";
-		
-	}
-	if ($method == "GET") {
-		print "Getting players resource";
-	}
-	
-}
-
- if ($resource == "newgame") {
+if ($resource == "newgame") {
 	if ($method == "POST") {
 		print "starting new game";
 		print "cards assignments";
 		header('HTTP/1.1 201 Created');
 	}
-} 
- if ($resource == "cards") {
-	if ($method == "GET") {
-		if (isset($_GET["playerid"])) {
-			$id = $_GET["playerid"];
-			
-		}else {
-			print "something wrong";
-			
-		}
-	}
-		
-		print "getting cards for player with id " . $id;
-		
-	}
+}
+
+if ($method == "POST") {
+	print "starting new game";
+	print "cards assignments";
+}
+
+//Resource for players
+if ($resource == "players") {
 	if ($method == "POST") {
-		print "starting new game";
-		print "cards assignments";
-	}
-	
-	
-	if ($resource == "setuser"){
-		if ($method == "POST"){
-			require_once "php/users.php";
-			print "posting player";
-			set_user($input);
-		}
-				
-} 
-
-	if ($resource == "getplayers"){
-		if ($method == "GET"){
-			require_once "php/users.php";
-			print "showing all player";
-			show_users();
-		}
-		
-		
-}
-
-
-if ($resource == "getplayer"){
-	if($method == "GET"){
 		require_once "php/users.php";
-		print "showing specific player";
-		show_user($input);
-		
+		add_user($requestBody);
 	}
-}
-		
-		
-
-if ($resource == "handleuser"){
-	require_once "php/users.php";
-	handle_user("POST",$playerid,$input);
-		 
-}
-
-
-if($resource=="resetgame"){
-	if ($method=="POST"){
-	require_once "php/board.php";
-	reset_game();
-	print "Game has been reseted";	
-		
+	if ($method == "DELETE") {
+		require_once "php/users.php";
+		deleteAllUsers();
+	}
+	if ($method == "GET") {
+		require_once "php/users.php";
+		if (isset($_GET['playerId'])) {
+			$id = $_GET['playerId'];
+			show_user($id);
+		} else {
+			show_users();
+		}	
 	}
 }
 
-if ($resource=="gamestatus"){
+//Resource for game
+if ($resource == "gamestatus") {
+
 	if ($method=="GET"){
-			require_once "php/board.php";
-			show_status();
-			echo nl2br ("Game status");
+		require_once "php/board.php";
+		show_status();
+	}if ($method=="DELETE") {
+		require_once "php/board.php";
+		reset_game();
 	}
 	
-}	
-
-if ($resource=="showcards"){
-	if($method=="GET"){
-		require_once "php/board.php";
-		show_cardgame($input);
-		echo nl2br ("Players with theese cards");
-	}
-}
-	
-	
-if ($resource=="drawcard"){
-	if ($method=="POST"){
-		require_once "php/board.php";
-		draw_card($input);
-		#echo nl2br ("draws card");
-	}
-}
-if ($resource=="showgame"){
-	if ($method=="POST"){
-		require_once "php/board.php";
-		show_game($input);
-	}
-}
 		
-	
+}
+
+if ($resource=="playtest"){
+	if ($method=="POST"){
+		require_once "php/board.php";
+		playtest($requestBody);
+	}
+}
+
+
+if ($resource=="play3cards"){
+	if ($method=="POST"){
+		require_once "php/board.php";
+		//$post_data = array();
+		//$posted_data = json_decode($_POST['cardCode'], true);
+		//$requestBody = $post_data;
+		//play3cards($requestBody);
+		$card1 = "";
+		$card2 = "";
+		$card3 = "";
+		$post_data=array();
+		$post_data = json_decode($_POST['json'], true);
+		$requestBody[] = $post_data;
+		///print_r($requestBody);
+		$card1 = implode($requestBody[0]);
+		$card2 = implode($requestBody[1]);
+		$card3 = implode ($requestBody[2]);
+		//echo implode("card1 \n",$requestBody[0]); 
+		//echo implode("card2 \n",$requestBody[1]);
+		//echo  implode("card3 \n",$requestBody[2]);
+
+
+		echo "card 1: \n " .$card1."\n";
+		echo "card 2: \n " . $card2."\n";
+		echo "card 3: \n " . $card3."\n";
+
+		play3cards($card1,$card2,$card3);
+
+	}
+
+
+}
+
+
+
+
+
+
+
+
+/*if ($resource=="play3cards"){
+	if ($method=="POST"){
+		require_once "php/board.php";
+		$posted_data = array();
+    if (!empty($_POST['json'])) {
+        $posted_data = json_decode($_POST['json'], true);
+			play3cards($posted_data);
+    }
+	}
+
+}*/
+
+
+//Resource for cards
+if ($resource == "cards") {
+	if ($method == "GET") {
+		require_once "php/board.php";
+		getallcards();
+
+	}
+	if (isset($_GET['playerId'])) {
+		$id = $_GET['playerId'];
+		showcardbyplayer($id);
+
+	}
+}
+
+if ($resource == "showcards") {
+	if ($method == "GET") {
+		require_once "php/board.php";
+		show_cardgame($requestBody);
+		echo nl2br("Players with theese cards");
+	}
+}
+
+
+if ($resource == "drawcard") {
+	if ($method == "POST") {
+		require_once "php/board.php";
+		draw_card($requestBody);
+	}
+}
+
+
+if ($resource == "showgame") {
+	if ($method == "POST") {
+		require_once "php/board.php";
+		show_game($requestBody);
+	}
+}
+
+
 
 
 
 
 //τελος γυρου και αλλγη σειρας
-if($resource=="endturn"){
-	if($method=="POST"){
+if ($resource == "endturn") {
+	if ($method == "POST") {
 		require_once "php/game.php";
 	}
 }
 
-if($resource=="pass"){
-	if($method=="POST"){
+if ($resource == "pass") {
+	if ($method == "POST") {
 		pass();
-}
-}
-
-function pass() {
-        global $mysqli;
-        $sql = 'call playerTurn()';
-        $st = $mysqli->prepare($sql);
-        $st -> execute();
-        
-        $sql = 'SELECT cardCode FROM discarded_cards WHERE number = (SELECT MAX(number) FROM discarded_cards)';
-	$sm = $mysqli -> prepare($sql);
-	$sm -> execute();
-	$rem = $sm -> get_result();
-	$ren = $rem -> fetch_assoc();
-
-	show_game($ren['cardCode']);
+	}
 }
 
 ?>
