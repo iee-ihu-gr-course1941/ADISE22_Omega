f971e9eca570ffb3eb3acaf007bef7a0d7abed65
kokoshkas007 <58370775+kokoshkas007@users.noreply.github.com>
2023-01-07 22:28:50 +0200

    Update dbconnect.php

--- php/dbconnect.php
+++ php/dbconnect.php
@@ -1,28 +1,20 @@
 <?php
-$host="localhost";
-$db="pinacle";
-$username="root";
-$pass="";
-$mysqli= mysqli_connect($host,$username,$pass,$db);
-mysqli_select_db($mysqli,"pinacle");
+$host='localhost';
+$db = 'pinacle';
+require_once "db_upass.php";
 
-    if (!$mysqli){
-        echo("<P> CONNECTION FAILED "."DATABASE SERVER IS NOT AVAILABLE </P>");
-		header('HTTP/1.1 503 Service Unavailable');
-        exit();
-    }else{
-		print "succefully connected";
-	}
+$user=$DB_USER;
+$pass=$DB_PASS;
 
-if(!mysqli_select_db($mysqli,"pinacle")){
-	echo "failed to connect db";
-	header('HTTP/1.1 503 Service Unavailable');
-	
-}else{
-	echo "database selected";
-	
+
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2013/it134156/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
 }
-	
 
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}?>
 
-?>
